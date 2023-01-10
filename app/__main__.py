0ae10895fa007e69cc002b5d6dad54e5e4d95a3a
Sclafus <alessandro.sclafani00@gmail.com>
2023-01-10 20:27:19 +0100

    initial commit

---
+++ app/__main__.py
@@ -0,0 +1,52 @@
+import csv
+from .playerinfo.PlayerInfo import PlayerInfo
+
+def get_data(filename: str = 'data.tsv'):
+    data = _load_data(filename)
+    return data
+
+
+def _load_data(filename: str = 'data.tsv'):
+    data = _load_raw_data(filename)
+    return transform_raw_data(data)
+
+
+def _load_raw_data(filename: str = 'data.tsv'):
+    with open(filename) as file:
+        tsv_file = csv.reader(file, delimiter="\t")
+        return [l for l in tsv_file]
+
+
+def transform_raw_data(data: str):
+    return [
+        PlayerInfo(
+            pp = int(line[1].replace(',', '')),
+            username = line[2],
+            city = line[3],
+            play_style = line[4],
+            tap_style = line[5],
+            tablet_mouse = line[6],
+            area_filter_sensitivity = line[7],
+            grip = line[8],
+            keyboard = line[9],
+            switch = line[10],
+            osu_resolution = line[11],
+            skin = line[12]
+        )
+    for line in data[1:]]
+
+def get_headers(filename: str = 'data.tsv'):
+    data = _load_raw_data(filename)
+    return data[0]
+
+def make_table(data: list[PlayerInfo]):
+    import tabulate
+    return tabulate.tabulate(data, headers=get_headers(), tablefmt='github')
+
+
+if __name__ == '__main__':
+    data = get_data()
+    table = make_table(data)
+
+    with open('README.md', 'w') as f:
+        f.write(table)
---
+++ app/playerinfo/PlayerInfo.py
@@ -0,0 +1,20 @@
+from dataclasses import astuple, dataclass
+
+@dataclass
+class PlayerInfo:
+    pp: int
+    username: str
+    city: str
+    play_style: str
+    tap_style: str
+    tablet_mouse: str
+    area_filter_sensitivity: str
+    grip: str
+    keyboard: str
+    switch: str
+    osu_resolution: str
+    skin: str
+
+
+    def __iter__(self):
+        return iter(astuple(self))   
